8fe7c62a208f5483a3d9ae65ae32ec92fa03e20f
Walisson Paulo <walisson200939@gmail.com>
2026-01-19 11:52:15 -0300

    Ajustando CompraService.

--- src/desafio06/service/CompraService.java
+++ src/desafio06/service/CompraService.java
@@ -6,31 +6,45 @@ import desafio06.model.Cliente;
 import desafio06.model.Produto;
 
 public class CompraService {
-    
 
-    public Cliente cadastrarCliente(Scanner sc){
+    public Cliente cadastrarCliente(Scanner sc) {
         Cliente cliente = null;
         String nomeCliente;
-        
 
         System.out.println("Para continuar cadaste um cliente!");
         System.out.println("Informe o nome do cliente:");
         nomeCliente = sc.nextLine();
-        
+
         return cliente;
     }
-    public Produto cadastraProduto(Scanner sc){
-        
-        System.out.println("Para continuar cadastre um produto!");
 
-        System.out.println("Qual o nome do produto?:");
-        String nomeProduto = sc.nextLine();
-        System.out.println("Qual o preço do produto?:");
-        Double precoProduto = sc.nextDouble();
-        sc.nextLine();
-        
+    public Produto cadastraProduto(Scanner sc) {
+
+        String nomeProduto;
+        Double precoProduto;
+        int opcao;
+
+        System.out.println("Para continuar cadastre um produto!");
+        System.out.println("");
+
+        do {
+            System.out.println("Qual o nome do produto?:");
+            nomeProduto = sc.nextLine();
+            System.out.println("");
+            System.out.println("Qual o preço do produto?:");
+            precoProduto = sc.nextDouble();
+            sc.nextLine();
+            System.out.println("");
+
+            System.out.println("Deseja cadastrar outro produto?");
+            System.out.println("1 - Sim");
+            System.out.println("2 - Não");
+            opcao = sc.nextInt();
+            sc.nextLine();
+        } while (opcao == 1);
+
         Produto produto = new Produto(nomeProduto, precoProduto);
-        
+
         return produto;
     }
 }
