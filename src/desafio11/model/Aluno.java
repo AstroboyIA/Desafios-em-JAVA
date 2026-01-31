6573ebf64aa529f4e4239b3ef7a053deb4c1986c
Walisson Paulo <walisson200939@gmail.com>
2026-01-30 23:54:48 -0300

    Começando escopo do desafio11.

---
+++ src/desafio11/model/Aluno.java
@@ -0,0 +1,13 @@
+package desafio11.model;
+
+public class Aluno {
+    String nome;
+
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ src/desafio11/model/Treino.java
@@ -0,0 +1,20 @@
+package desafio11.model;
+
+public class Treino {
+    String descricao;
+    int duracaoMinutos;
+    
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Treino(String descricao, int duracaoMinutos) {
+        this.descricao = descricao;
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
+}
