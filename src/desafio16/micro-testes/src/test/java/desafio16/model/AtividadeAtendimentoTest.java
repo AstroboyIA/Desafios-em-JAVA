cb882d7ae40978b9a815456d5d18cdf36f8c6ba9
Walisson Paulo <walisson200939@gmail.com>
2026-02-19 13:03:03 -0300

    Já não sei mais o que estou fazendo.

---
+++ src/desafio16/micro-testes/src/test/java/desafio16/model/AtividadeAtendimentoTest.java
@@ -0,0 +1,28 @@
+package desafio16.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import main.desafio16.model.AtividadeAtendimento;
+
+public class AtividadeAtendimentoTest {
+
+    @Test
+    void deveCriarAtividadeValida() {
+        AtividadeAtendimento atividade = new AtividadeAtendimento("Instalar software", 60, 30);
+
+        assertEquals(60, atividade.getMinutosEstimados());
+    }
+
+    @Test
+    void deveLancarExcecaoParaEstimadoInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> new AtividadeAtendimento("Teste", 0, 0));
+    }
+
+    @Test
+    void deveLancarExcecaoParaExecutadoMaiorQueEstimado() {
+        assertThrows(IllegalArgumentException.class, () -> new AtividadeAtendimento("Teste", 10, 20));
+    }
+}
